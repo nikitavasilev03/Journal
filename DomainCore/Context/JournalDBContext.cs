4cde62098c8b379a24b91a375dc9bf81dd5d8e96
Никита Васильев <nikitavasilev2303@gmail.com>
2020-05-22 11:41:52 +0300

    версия 0.2.1 fix

--- DomainCore/Context/JournalDBContext.cs
+++ DomainCore/Context/JournalDBContext.cs
@@ -9,6 +9,7 @@ namespace DomainCore.Context
 {
     public partial class JournalDBContext : DbContext
     {
+        public static string ConnectionString { get; set; }
         public JournalDBContext()
         {
         }
@@ -31,7 +32,7 @@ namespace DomainCore.Context
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=JournalDB;Trusted_Connection=True;" + "MultipleActiveResultSets=True");
+                optionsBuilder.UseSqlServer(ConnectionString);
             }
             
         }
--- WebAppMVC/Startup.cs
+++ WebAppMVC/Startup.cs
@@ -21,6 +21,9 @@ namespace WebAppMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Устанавливаем строку подключения для БД поумолчанию
+            JournalDBContext.ConnectionString = Configuration.GetConnectionString("DefaultConnection");
+
             services.AddDbContext<JournalDBContext>();
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
